6b3856c367a99c88468acbaf8e3bc791f8ac646d
wazhang <wangchun.zhang@ge.com>
2017-05-28 02:53:09 -0700

    checkin the code

--- server.R
+++ server.R
@@ -14,9 +14,20 @@ library(datasets)
 library(ggplot2)
 
 shinyServer(function(input, output, session) {
+  output$dtype <- reactive({
+    if(input$datasets == '')
+      NULL
+    else{
+      dsString <- str_replace_all(unlist(strsplit(input$datasets, '\\('))[1], pattern = "\\s+", "")
+      data<-get(dsString)
+      class(data)
+  }})
+  
+  outputOptions(output, "dtype", suspendWhenHidden = FALSE)  
+  
   observe({
     if(input$datasets == '') {
-      c<-c("mtcars", "iris" ) #data()$results[, "Item"]
+      c<-c("mtcars", "iris", "volcano", "AirPassengers", "mdeaths", "treering", "uspop" ) #data()$results[, "Item"]
       updateSelectInput(session, "datasets", choices=c, selected=FALSE)  
     }
     else {
@@ -25,108 +36,142 @@ shinyServer(function(input, output, session) {
       dataCls <- class(data)
       
       if(dataCls == 'data.frame') {
+        fac<-c()
+        for(name in names(data)) {
+          if(length(unique(data[[name]])) < 10)
+            fac<-c(fac, name)
+        }
+        
         updateNumericInput(session, "header_rows", max=dim(data)[1])
-        print("updateAttributes")
         updateSelectInput(session, "attr1", choices=names(data), selected=FALSE)  
+        updateSelectInput(session, "attr11", choices=names(data), selected=FALSE)
+        updateSelectInput(session, "attr12", choices=names(data), selected=FALSE)  
+        updateSelectInput(session, "attr13", choices=fac, selected=FALSE)         
         updateSelectInput(session, "attr21", choices=names(data), selected=FALSE)
         updateSelectInput(session, "attr22", choices=names(data), selected=FALSE)  
-        updateSelectInput(session, "attr23", choices=names(data), selected=FALSE)  
+        updateSelectInput(session, "attr23", choices=fac, selected=FALSE)  
       }
     }
   })
   
   output$ui<- renderUI({
-    if(input$datasets == '')
-      return()
-    
-    tags <- c();
-    
-    dsString <- str_replace_all(unlist(strsplit(input$datasets, '\\('))[1], pattern = "\\s+", "")
-    data<-get(dsString);
-    
-    if(input$dtype) {
-      tags<-c(tags, renderPrint(class(data)))
+    tags <- c()
+    if(input$datasets == '') {
+      tages<-c(tags, renderPrint("Exploratory data analaysis content"))
+      tagList(tags)
     }
-    
-    if(input$varType) {
-      tags<-c(tags, renderPrint(lapply(data, class)))
-    }
-    
-    dataCls <- class(data)
-    if(input$dimension){ ## render dimension portion
-      if(dataCls == 'data.frame' || dataCls == 'matrix' || dataCls == 'array')
-        tags <- c(tags, renderPrint(dim(data)))  
-      else {
-        tags <- c(tags, renderPrint('No dimension for non-tabular dataset or array'))  
+    else {
+      dsString <- str_replace_all(unlist(strsplit(input$datasets, '\\('))[1], pattern = "\\s+", "")
+      data<-get(dsString)
+      
+      if(input$dtype) {
+        tags<-c(tags, renderPrint(class(data)))
       }
-    }
-    
-    if(input$header){
-      if(dataCls == 'data.frame' || dataCls == 'matrix' || dataCls == 'array'){
-        rows <-input$header_rows
-        if(rows == '' || rows >dim(data)[1] )
-          rows <- dim(data)[1]
-        
-        tag<-renderPrint(head(data, rows))
-        tags <- c(tags, tag)
-      }
-      else {
-        if(dataCls == 'ts') {
-           tags <- c(tags, renderText('For time series data, show first 100 data'))  
-           tags <- c(tags,  renderPrint({data[1:100]}))  
+      
+      if(input$varType) {
+        if(class(data)=='data.frame') {
+          tags<-c(tags, renderPrint(sapply(data, class)))
         }
-        else{
-          tags <- c(tags, renderPrint('No header information for non-tabular dataset or array'))  
+        else if(class(data)=='ts' || class(data) == 'matrix'){
+           tags<-c(tags, renderPrint(unique(sapply(data, class))))
         }
       }
-    }
-    
-    if(input$summary) {
-      tag<-renderPrint(summary(data))
-      tags <- c(tags, tag)
-    }
-    
-    if(input$histrogram){
-      if(dataCls == 'data.frame'){
-        if(input$attr1 != ''){
-          tag<-renderPlot({
-            apply(data[input$attr1][1],2, hist)
-          })
+      
+      dataCls <- class(data)
+      if(input$dimension){ ## render dimension portion
+        if(dataCls == 'data.frame' || dataCls == 'matrix' || dataCls == 'array')
+          tags <- c(tags, renderPrint(dim(data)))  
+      }
+      
+      if(input$header){
+        if(dataCls == 'data.frame' || dataCls == 'matrix' || dataCls == 'array'){
+          rows <-input$header_rows
+          if(rows == '' || rows >dim(data)[1] )
+            rows <- dim(data)[1]
           
+          tag<-renderPrint(head(data, rows))
           tags <- c(tags, tag)
         }
       }
-    }
-    
-    
-    if(input$cooralation) {
-      if(dataCls == 'data.frame'){
-        if(input$attr21 != '' && input$attr22 != ''){
-          if(input$attr23 != ''){
-            selectedData<- data[, c(input$attr21, input$attr22, input$attr23)]
+      
+      if(input$summary) {
+        tag<-renderPrint(summary(data))
+        tags <- c(tags, tag)
+      }
+      
+      if(input$histrogram){
+        if(dataCls == 'data.frame'){
+          if(input$attr1 != ''){
             tag<-renderPlot({
-              ggplot(selectedData, aes(x=selectedData[[input$attr21]], y=selectedData[[input$attr22]])) +
-                geom_point(size=6,aes(colour = factor(selectedData[[input$attr23]]))) +
+              apply(data[input$attr1][1],2, hist)
+            })
+            
+            tags <- c(tags, tag)
+          }
+        }
+      }
+      
+      if(input$scatterplot && class(data)=='data.frame') {
+        if(input$attr11 != '' && input$attr12 != ''){
+          if(input$attr13 != ''){
+            sd<- data[, c(input$attr11, input$attr12, input$attr13)]
+            tag<-renderPlot({
+              ggplot(sd, aes(x=sd[[input$attr11]], y=sd[[input$attr12]])) +
+                geom_boxplot(aes(fill= factor(sd[[input$attr13]]))) +
                 xlab(input$attr21) +
                 ylab(input$attr22)
             })
             tags <- c(tags, tag)
           }
           else{
-            selectedData<- data[, c(input$attr21, input$attr22)]
+            sd<- data[, c(input$attr11, input$attr12)]
             tag<-renderPlot({
-              ggplot(selectedData, aes(x=selectedData[[input$attr21]], y=selectedData[[input$attr22]])) +
-                geom_point(size=6) +
-                xlab(input$attr21) +
-                ylab(input$attr22)
+              ggplot(sd, aes(x=sd[[input$attr11]], y=sd[[input$attr12]])) +
+                geom_boxplot() +
+                xlab(input$attr11) +
+                ylab(input$attr12)
             })
             tags <- c(tags, tag)
+          }          
+          
+
+        }
+      }
+      
+      if(input$tsplot && class(data)=='ts'){
+         tag<-renderPlot({plot(data)})
+         tags <- c(tags, tag)
+      }
+      
+      if(input$cooralation) {
+        if(dataCls == 'data.frame'){
+          if(input$attr21 != '' && input$attr22 != ''){
+            if(input$attr23 != ''){
+              selectedData<- data[, c(input$attr21, input$attr22, input$attr23)]
+              tag<-renderPlot({
+                ggplot(selectedData, aes(x=selectedData[[input$attr21]], y=selectedData[[input$attr22]])) +
+                  geom_point(size=6,aes(colour = factor(selectedData[[input$attr23]]))) +
+                  xlab(input$attr21) +
+                  ylab(input$attr22)
+              })
+              tags <- c(tags, tag)
+            }
+            else{
+              selectedData<- data[, c(input$attr21, input$attr22)]
+              tag<-renderPlot({
+                ggplot(selectedData, aes(x=selectedData[[input$attr21]], y=selectedData[[input$attr22]])) +
+                  geom_point(size=6) +
+                  xlab(input$attr21) +
+                  ylab(input$attr22)
+              })
+              tags <- c(tags, tag)
+            }
           }
         }
       }
-    }
-    
-    tagList(tags)
+      
+      tagList(tags)      
+    }      
   })      
 })
   
--- ui.R
+++ ui.R
@@ -15,22 +15,56 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       ## Select the datasource
-      selectInput("datasets", "Select dataset", choices=NULL),
-      
+      selectInput("datasets", "Pleases select a dataset", choices=NULL),
+
       fluidRow(column(width=10, checkboxInput("dtype", "Dataset type", FALSE))),
+      
       fluidRow(column(width=10, checkboxInput("varType", "Variable type", FALSE))),
-      fluidRow(column(width=10, checkboxInput("dimension", "Dimension", FALSE))),
-      fluidRow(column(width=5,  checkboxInput("header", "Header", FALSE)),
-               column(width=5,numericInput("header_rows", "Rows?", value=5, min=0))),
+      
+      fluidRow(column(width=10, 
+                      conditionalPanel( condition = "output.dtype != 'ts'",
+                                        checkboxInput("dimension", "Dimension", FALSE)))),
+      fluidRow(column(width=5,  
+                      conditionalPanel( condition = "output.dtype != 'ts'",
+                                        checkboxInput("header", "Header", FALSE))),
+               column(width=5, 
+                      conditionalPanel( condition = "output.dtype != 'ts'",
+                                        numericInput("header_rows", "Rows?", value=5, min=0)))),
       fluidRow(column(width=10,  checkboxInput("summary", "Summary", FALSE))),
-      fluidRow(column(width=5, checkboxInput("histrogram", "Histrogram", FALSE)),
-               column(width=5,selectInput("attr1", "Attribute", choices=NULL))),
+      fluidRow(column(width=5, conditionalPanel( condition = "output.dtype=='data.frame'",
+                      checkboxInput("histrogram", "Histrogram", FALSE))),
+               column(width=5, conditionalPanel( condition = "output.dtype=='data.frame'",
+                      selectInput("attr1", "Attribute", choices=NULL)))),
       
-      fluidRow(column(width=10, checkboxInput("cooralation", "Correlation", FALSE))),
+      fluidRow(column(width=5,
+                      conditionalPanel(condition = "output.dtype == 'ts'",
+                                       checkboxInput("tsplot", "Time Series plot", FALSE)))),
       
-      fluidRow(column(width=4,selectInput("attr22", "Y attribute", choices=NULL)),
-               column(width=4,selectInput("attr21", "X attribute", choices=NULL)),
-               column(width=4,selectInput("attr23", "Color attr", choices=NULL)))
+      fluidRow(column(width=10, 
+                      conditionalPanel(condition = "output.dtype == 'data.frame'",
+                                       checkboxInput("scatterplot", "Box Plot", FALSE)))),
+      fluidRow(column(width=4,
+                      conditionalPanel(condition = "output.dtype == 'data.frame'",
+                                       selectInput("attr12", "Y attribute", choices=NULL))),
+               column(width=4,
+                      conditionalPanel(condition = "output.dtype == 'data.frame'",
+                                       selectInput("attr11", "X attribute", choices=NULL))),
+               column(width=4,
+                      conditionalPanel(condition = "output.dtype == 'data.frame'",
+                                       selectInput("attr13", "Color attr", choices=NULL)))),
+      
+      fluidRow(column(width=10, 
+                      conditionalPanel(condition = "output.dtype == 'data.frame'",
+                                       checkboxInput("cooralation", "Correlation", FALSE)))),
+      fluidRow(column(width=4,
+                      conditionalPanel(condition = "output.dtype == 'data.frame'",
+                                       selectInput("attr22", "Y attribute", choices=NULL))),
+               column(width=4,
+                      conditionalPanel(condition = "output.dtype == 'data.frame'",
+                                       selectInput("attr21", "X attribute", choices=NULL))),
+               column(width=4,
+                      conditionalPanel(condition = "output.dtype == 'data.frame'",
+                                       selectInput("attr23", "Color attr", choices=NULL))))
     ),
     
     mainPanel(
